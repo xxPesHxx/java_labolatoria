8340b5549a2745160a58ff3824dcf733334aba74
Kacper Waszczuk <waszczuk66@gmail.com>
2024-02-28 14:15:46 +0100

    123

--- project/src/Main.java
+++ project/src/Main.java
@@ -4,17 +4,38 @@
 
 public class Main {
 
+//    static Segment perpendictularTo(Segment segment1, Point point1)
+//    {
+//        float dx = segment1.getEnd().x - segment1.getStart().x;
+//        float dy = segment1.getEnd().y - segment1.getStart().y;
+//        //return new Segment(p, )
+//
+//        return new Segment[] {
+//            point1.x- dy, point1.y + dx,
+//            point1.x - dx, point1.y + dx,
+//        }
+//    }
+
     public static void main(String[] args) {
 
 
-        Point point;
-        point = new Point(2.5F, 3.7F);
-        System.out.println(point);
+//        Point point;
+//        point = new Point(2.5F, 3.7F);
+//        System.out.println(point);
+//
+//        Segment segment;
+//        segment = new Segment(point, new Point(5.7F, 2.8F));
+//
+//        System.out.println(segment.toSvg());
+//        System.out.println(segment.distance());
 
-        Segment segment;
-        segment = new Segment(point, new Point(5.7F, 2.8F));
+        Polygon poly = new Polygon(new Point[]{
+                new Point (30, 70),
+                new Point (50,80),
+                new Point (70,100)
+        });
 
-        System.out.println(segment.toSvg());
-        //System.out.println(segment.distance());
+
+
     }
 }
--- project/src/Segment.java
+++ project/src/Segment.java
@@ -28,6 +28,5 @@ public class Segment
     {
         return String.format(Locale.ENGLISH, "<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" style=\"stroke:red;stroke-width:2\" />", start.x, start.y, end.x, end.y);
     }
-    //<line x1="0" y1="0" x2="300" y2="200" style="stroke:red;stroke-width:2" />
 
 }
